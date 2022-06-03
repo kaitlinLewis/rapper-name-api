e7a897921b9b65bffa655da1b6c0a8a5f7a83bb2
kaitlinLewis <kaitlinlewis.dev@gmail.com>
2022-06-02 21:23:28 -0400

    server side js and cors

---
+++ index.js
@@ -0,0 +1,16 @@
+document.querySelector("button").addEventListener("click", apiRequest);
+
+async function apiRequest() {
+  const rapperName = document.querySelector("input").value;
+  try {
+    const response = await fetch(
+      `https://api-rapper-names.herokuapp.com/api/${rapperName}`
+    );
+    const data = await response.json();
+
+    console.log(data);
+    document.querySelector("h2").innerText = data.birthName;
+  } catch (error) {
+    console.log(error);
+  }
+}
