1c7380638b3563269562f98bc19b519dedc409f9
kaitlinLewis <kaitlinlewis.dev@gmail.com>
2022-06-02 21:12:31 -0400

    env PORT

--- server.js
+++ server.js
@@ -33,6 +33,6 @@ app.get("/api/:name", (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || PORT, () => {
   console.log(`The server is now running on port ${PORT}! Better go catch it!`);
 });
